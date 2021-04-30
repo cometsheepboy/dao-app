e1278deabadb64855ca210c5f07b5ba71f76a97b
cometsheepboy <cometsheepboy@gmail.com>
2021-04-30 21:34:23 +0545

    implemented ERC20 interface

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-contract ERC20 {
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
     uint private _totalSupply;
     
     mapping(address => uint) private _balances;
+    mapping(address => mapping(address => uint)) private _allowances;
+    
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
     
     constructor(string memory name_, string memory symbol_, uint8 decimals_) {
         _name = name_;
@@ -37,6 +43,31 @@ contract ERC20 {
         assert(_balances[_to] + _value > _balances[_to]);
         _balances[msg.sender] -= _value;
         _balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(msg.sender != address(0), "FROM_ZERO_ADDRESS");
+        require(_spender != address(0), "TO_ZERO_ADDRESS");
+
+        _allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowances[_owner][_spender];
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_balances[_from] >= _value, "NOT_ENOUGH_FUNDS");
+        require(_allowances[_from][_to] >= _value, "NOT_ALLOWED");
+        assert(_balances[_from] > _balances[_from] - _value);
+        assert(_balances[_to] + _value > _balances[_to]);
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
+        _allowances[_from][_to] -= _value;
         return true;
     }
 }
