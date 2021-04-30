980da31c50f97653b392f2b771577c33c71a9394
cometsheepboy <cometsheepboy@gmail.com>
2021-05-01 00:46:04 +0545

    add emitCoint fn

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -5,18 +5,41 @@ import "./IERC20.sol";
 import "./ERC20.sol";
 
 contract DAO {
-    string private _name;
+    string public name;
     uint private _totalEth;
     
     struct Partner {
-        IERC20 token;
         address referrer;
         uint balance;
         mapping(address => uint) price;
+        mapping(bytes3 => IERC20) tokens;
     }
 
-    constructor(string memory name_) {
-        _name = name_;
+    mapping(address => Partner) private _partners;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+
+    modifier onlyPartner() {
+        require(_partners[msg.sender].referrer != address(0), "NOT_REGISTERED");
+        _;
+    }
+
+    function register(address _referrer) external payable returns(bool) {
+        require(_referrer != address(0), "REFERRER_REQUIRED");
+        require(msg.value >= 1 ether, "NOT_ENOUGH_ETH");
+        _partners[msg.sender].referrer = _referrer;
+        _totalEth += msg.value;
+        return true;
+    }
+
+    function emitCoin(string memory _name, bytes3 _symbol) external payable onlyPartner returns(bool) {
+        require(msg.value >= 0.1 ether);
+        assert(_totalEth + 0.1 ether > _totalEth);
+        _totalEth += 0.1 ether;
+        _partners[msg.sender].tokens[_symbol] = new ERC20(_name, string(abi.encodePacked(_symbol)), 18, msg.sender);
+        return true;
     }
     
     receive() external payable {
