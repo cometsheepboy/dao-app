37e68d9445df7a949438c1f1b5313d84eabbdf3e
cometsheepboy <cometsheepboy@gmail.com>
2021-04-30 13:22:09 +0545

    add Owner contract

---
+++ contracts/Owner.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract Owner {
+
+    address public owner;
+    address[] private _ownerHistory;
+
+    event OwnerChanged(address indexed _from, address indexed _to);
+    event OwnerHistory(address[] _history);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(_isOwner(), "MOD_ONLY_OWNER");
+        _;
+    }
+
+    function getOwnerHistory() external {
+        emit OwnerHistory(_ownerHistory);
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+        emit OwnerChanged(msg.sender, _newOwner);
+    }
+
+    function _isOwner() internal view returns(bool) {
+        return msg.sender == owner;
+    }
+    
+}
