f666602443cd02e52b6c8f4892da2853727efdcd
cometsheepboy <cometsheepboy@gmail.com>
2021-04-30 12:47:03 +0545

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import getWeb3 from "./getWeb3";
+import "./App.css";
+
+function App() {
+  return (<div className="container"></div>)
+}
+
+export default App;
