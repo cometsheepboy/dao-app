c1aafba6d7dbc2bd1e61d560f7b14819e44e4020
cometsheepboy <cometsheepboy@gmail.com>
2021-05-05 06:20:14 +0400

    update frontend template

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import getWeb3 from "./getWeb3";
-import "./App.css";
+import getWeb3 from "../../getWeb3";
+import "./app.css";
 
 function App() {
   return (<div className="container"></div>)
